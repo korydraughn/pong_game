c557f724d2c335f01eca9c9c3e17dd699816c179
Kory Draughn <korydraughn@ymail.com>
2018-09-21 18:41:12 -0400

    Added keyboard support and clang-format style/config.

--- Classes/pong.cpp
+++ Classes/pong.cpp
@@ -1,18 +1,18 @@
 /****************************************************************************
  Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
+
  http://www.cocos2d-x.org
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,84 +22,61 @@
  THE SOFTWARE.
  ****************************************************************************/
 
-#include "HelloWorldScene.h"
+#include "pong.hpp"
 #include "SimpleAudioEngine.h"
 
 USING_NS_CC;
 
-Scene* HelloWorld::createScene()
+Scene* pong::createScene()
 {
-    return HelloWorld::create();
-}
-
-// Print useful error message instead of segfaulting when files are not there.
-static void problemLoading(const char* filename)
-{
-    printf("Error while loading: %s\n", filename);
-    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+    return pong::create();
 }
 
 // on "init" you need to initialize your instance
-bool HelloWorld::init()
+auto pong::init() -> bool
 {
     //////////////////////////////
     // 1. super init first
     if (!Scene::init())
         return false;
 
-    //auto visibleSize = Director::getInstance()->getVisibleSize();
-    //Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    // auto visibleSize = Director::getInstance()->getVisibleSize();
+    // Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
     // Draw paddles.
-    auto* left_paddle = DrawNode::create();
-    if (left_paddle)
+    l_paddle_ = DrawNode::create();
+    if (l_paddle_)
     {
         Vec2 r_origin{};
         Vec2 r_dst{100, 100};
         Color4F r_color{1, 1, 1, 1};
-        left_paddle->drawSolidRect(r_origin, r_dst, r_color);
-        addChild(left_paddle, 0);
+        l_paddle_->drawSolidRect(r_origin, r_dst, r_color);
+        addChild(l_paddle_, 0);
     }
 
     // Add keyboard support.
     auto* listener = EventListenerKeyboard::create();
 
-    listener->onKeyPressed = [](EventKeyboard::KeyCode _key_code, Event* _event)
-    {
+    listener->onKeyPressed = [](EventKeyboard::KeyCode _key_code, Event* _event) {
         log("Key with keycode %d pressed", _key_code);
-
-        auto* target = _event->getCurrentTarget();
-
-        switch (_key_code)
-        {
-            case EventKeyboard::KeyCode::KEY_UP_ARROW:
-                target->setPositionY(target->getPositionY() + 1);
-                //target->runAction(MoveBy::create(1, {0.f, 4.f}));
-                break;
-
-            case EventKeyboard::KeyCode::KEY_DOWN_ARROW:
-                target->setPositionY(target->getPositionY() - 1);
-                //target->runAction(MoveBy::create(1, {0.f, -4.f}));
-                break;
-
-            default:
-                break;
-        }
+        key_state_[static_cast<int>(_key_code)] = true;
     };
 
-    listener->onKeyReleased = [](auto _key_code, auto* _event)
-    {
+    listener->onKeyReleased = [](auto _key_code, auto* _event) {
         log("Key with keycode %d released", _key_code);
+        key_state_[static_cast<int>(_key_code)] = false;
     };
 
-    Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, left_paddle);
+    Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, this);
+
+    scheduleUpdate();
 
     return true;
 }
 
-void HelloWorld::menuCloseCallback(Ref* pSender)
+auto pong::menuCloseCallback(Ref* pSender) -> void
 {
-    //Close the cocos2d-x game scene and quit the application
+    // Close the cocos2d-x game scene and quit the application
     Director::getInstance()->end();
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
@@ -110,6 +87,40 @@ void HelloWorld::menuCloseCallback(Ref* pSender)
      * do not use Director::getInstance()->end() and exit(0) as given above,instead trigger
      * a custom event created in RootViewController.mm as below*/
 
-    //EventCustom customEndEvent("game_scene_close_event");
+    // EventCustom customEndEvent("game_scene_close_event");
     //_eventDispatcher->dispatchEvent(&customEndEvent);
+}
+
+auto pong::update(float _delta_time) -> void
+{
+    Node::update(_delta_time);
+
+    if (!l_paddle_)
+        return;
+
+    auto speed = 50.f * _delta_time;
+    auto pos = l_paddle_->getPosition();
+
+    if (is_key_pressed(EventKeyboard::KeyCode::KEY_LEFT_ARROW))
+    {
+        l_paddle_->setPositionX(pos.x - speed);
+    }
+    else if (is_key_pressed(EventKeyboard::KeyCode::KEY_RIGHT_ARROW))
+    {
+        l_paddle_->setPositionX(pos.x + speed);
+    }
+
+    if (is_key_pressed(EventKeyboard::KeyCode::KEY_UP_ARROW))
+    {
+        l_paddle_->setPositionY(pos.y + speed);
+    }
+    else if (is_key_pressed(EventKeyboard::KeyCode::KEY_DOWN_ARROW))
+    {
+        l_paddle_->setPositionY(pos.y - speed);
+    }
+}
+
+auto pong::is_key_pressed(EventKeyboard::KeyCode _key_code) -> bool
+{
+    return key_state_[static_cast<int>(_key_code)];
 }
--- Classes/pong.hpp
+++ Classes/pong.hpp
@@ -1,18 +1,18 @@
 /****************************************************************************
  Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
+
  http://www.cocos2d-x.org
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,23 +22,33 @@
  THE SOFTWARE.
  ****************************************************************************/
 
-#ifndef __HELLOWORLD_SCENE_H__
-#define __HELLOWORLD_SCENE_H__
+#ifndef __PONG_HPP__
+#define __PONG_HPP__
 
 #include "cocos2d.h"
 
-class HelloWorld : public cocos2d::Scene
+#include <array>
+
+class pong : public cocos2d::Scene
 {
 public:
     static cocos2d::Scene* createScene();
 
-    virtual bool init();
-    
-    // a selector callback
+    CREATE_FUNC(pong);
+
+    bool init() override;
+
+    void update(float _delta_time) override;
+
     void menuCloseCallback(cocos2d::Ref* pSender);
-    
-    // implement the "static create()" method manually
-    CREATE_FUNC(HelloWorld);
+
+private:
+    static auto is_key_pressed(cocos2d::EventKeyboard::KeyCode _key_code) -> bool;
+
+    cocos2d::DrawNode* l_paddle_{};
+    cocos2d::DrawNode* r_paddle_{};
+
+    inline static std::array<bool, 164> key_state_{};
 };
 
-#endif // __HELLOWORLD_SCENE_H__
+#endif // __PONG_HPP__
